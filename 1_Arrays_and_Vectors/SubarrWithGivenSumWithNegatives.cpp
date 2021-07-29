7e5d38edcf7e0b59385971c5c7d930880e9174ae
akormous <iamakshatchauhan@gmail.com>
2021-07-29 14:54:33 +0530

    Day 8 of DSA

--- 1_Arrays_and_Vectors/SubarrWithGivenSumWithNegatives.cpp
+++ 1_Arrays_and_Vectors/SubarrWithGivenSumWithNegatives.cpp
@@ -1,45 +1,59 @@
 /*
 Find if an array contains a subarr with given sum
+Calculate number of subarrays with given sum
 NOTE: The array can contain negative numbers as well 
 */
+// { Driver Code Starts
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
-void solution(vector<int>& arr, int x) {
-    // size of arr
-    int n = arr.size();
-    // length of subarr with given sum
-    int answer;
-    // a hashmap that store prefix sum
-    unordered_map<int,int> umap;
-    /* Approach: store prefix sum in a hashmap,
-        check if umap[i] - sum is already present in the hashmap
-        if it is present, it means that a subarr with given sum is present
-        (similar to zero sum subarr problem)
-    */
-    int sum = 0;
-    for(int i = 0; i < n; i++) {
-        sum += arr[i];
-        // check if current sum is equal to the given sum
-        if(sum == x) {
-            cout<<"Subarray with sum "<<x<<" is present between 0 and "<<i<<endl;
-            return;
-        }
-        // check if sum - x is present in the hashmap
-        if(umap.find(sum - x) != umap.end()) {
-            cout<<"Subarray with sum "<<x<<" is present between "<<umap[sum]<<" and "<<i<<endl;
-            return;
-        }
+ // } Driver Code Ends
+class Solution
+{
+  public:
+    int subArraySum(int arr[], int n, int sum)
+    {
+    	// a hashmap that stores the number of time a sum has been encountered.
+    	unordered_map<int,int> umap;
 
-        umap[sum] = i;
+    	int cur_sum = 0;
+        // number of subarrays aka answer
+    	int count = 0;
+    	
+    	for(int i = 0; i < n; i++) {
+    	    cur_sum += arr[i];
+
+    	    if(cur_sum == sum)
+    	        count++;
+
+    	    if(umap.find(cur_sum - sum) != umap.end())
+    	        count += umap[cur_sum - sum];
+    	    
+            // increase the frequency of a particular sum
+    	    umap[cur_sum]++;
+    	}
+    	return count;
     }
+};
 
-}
-
-int main() {
-    vector<int> arr = {10, 2, -2, -20, 10};
-    int x = -10;
-    solution(arr, x);
-    cout<<endl;
-    return 0;
-}
+// { Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,sum=0;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    cin>>arr[i];
+	    cin>>sum;
+	    Solution ob;
+	    cout<<ob.subArraySum(arr, n, sum);
+	    cout<<'\n';
+	}
+	return 0;
+}  // } Driver Code Ends
