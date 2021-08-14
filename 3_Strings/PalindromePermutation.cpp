eb278be74e1c8753bb09fc777258cf8915065c1c
akormous <iamakshatchauhan@gmail.com>
2021-08-14 21:13:10 +0530

    Day 18 of DSA

--- 3_Strings/PalindromePermutation.cpp
+++ 3_Strings/PalindromePermutation.cpp
@@ -12,6 +12,66 @@ Taco cat
 
 */
 
+
+string removeWhiteSpaces(string s) {
+    // removing all whitespaces
+    int i = 0;
+    while(i < s.length()) {
+        if(s[i] == ' ')
+            s.erase(s.begin() + i);
+        else 
+            i++;
+    }
+    return s;
+}
+
+string toLowerCase(string s) {
+    transform(s.begin(), s.end(), s.begin(), ::tolower);
+    return s;
+}
+
+/*
+
+Best Approach - Bit vector
+in a palindrome the frequency of each character should be even,
+this can be checked by using a bit vector as on-off machine
+initially all of them are off,
+so if the frequency of all characters is even
+then all of them should be off in the final state
+
+except when the length of the string is odd,
+so only 1 can remain toggled
+
+NOTE: always keep in mind the character set of the string, ASCII or Unicode
+For now, i am considering only a - z
+
+Bit toggle - xor
+
+*/
+
+void toggle(int &bitVector, char c) {
+    bitVector = bitVector ^ (1 << (c - 'a'));
+}
+
+bool isPalindromePermutation(string s) {
+    cout<<"Bit vector implementation"<<endl;
+    // remove all whitespaces
+    s = removeWhiteSpaces(s);
+
+    // convert string to lowercase
+    s = toLowerCase(s);
+
+    // an int bit vector can handle upto 32 characters
+    // initially all bits are off
+    int bitVector = 0;
+    // traverse each character in the string and toggle it
+    for(int i = 0; i < s.length(); i++)
+        toggle(bitVector, s[i]);
+    
+    return bitVector == 0 || (bitVector & (bitVector - 1)) == 0;
+}
+
+
 /*
 Palindrome is a string that is read same backwards and forwards
 
@@ -20,7 +80,7 @@ Input -
 - can contain whitespaces
 - can contain ASCII characters
 
-Approach -
+Approach - Using a frequency table
 - Remove all whitespaces
 - convert to lowercase or uppercase
 
@@ -40,45 +100,45 @@ Time Complexity - O(N)
 Space Complexity - O(1) because the character set can be constant ? ASCII or Unicode
 */
 
-string removeWhiteSpaces(string s) {
-    // removing all whitespaces
-    int i = 0;
-    while(i < s.length()) {
-        if(s[i] == ' ')
-            s.erase(s.begin() + i);
-        else 
-            i++;
-    }
-    return s;
-}
-
-bool isPalindromePermutation(string s) {
-    if(s.length() < 2)
-        return false;
-    // remove whitespaces
-    s = removeWhiteSpaces(s);
-    
-    // covert to lowercase
-    transform(s.begin(), s.end(), s.begin(), ::tolower);
-    
-
-    // create a freq table
-    int freq[128];
-    memset(freq, 0, sizeof(freq));
-
+void getFrequency(int freq[], string s) {
     // traverse the string and update frequencies
     for(int i = 0; i < s.length(); i++) {
         freq[s[i]]++;
     }
+}
+
+bool isPalindromePermutation2(string s) {
+    // empty string
+    if(s.length() < 1)
+        return false;
+
+    // only one character
+    if(s.length() == 1)
+        return true;
+    
+    // remove whitespaces
+    s = removeWhiteSpaces(s);
+    // covert to lowercase
+    transform(s.begin(), s.end(), s.begin(), ::tolower);
+    
+    // create a freq table
+    int freq[128];
+    memset(freq, 0, sizeof(freq));
+
+    // get frequencies
+    getFrequency(freq, s);
 
     // incase if the number of characters in the string are odd
     // then a flag will keep track of the if that character is checked or not
     bool pe = false;
     for(int i = 0; i < 128; i++) {
         if(freq[i] % 2 != 0) {
-            if(pe == false)
+            // if the string length is odd
+            if( (s.length() % 2) != 0 && !pe) {
                 pe = true;
-            else
+            }
+            // else freq is not even
+            else 
                 return false;
         }
     }
@@ -88,7 +148,7 @@ bool isPalindromePermutation(string s) {
 
 int main() {
     // solution
-    if(isPalindromePermutation("C a R e C a R    "))
+    if(isPalindromePermutation("A b R B adf sf  h d s h r"))
         cout<<"Yes";
     else
         cout<<"No";
