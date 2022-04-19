6735323b0bcd849b4f55d6efc88ab14ced5d4a3e
akormous <iamakshatchauhan@gmail.com>
2022-04-19 10:03:55 +0530

    Update RecoverBST

--- 5_Trees_and_Graphs/RecoverBST.cpp
+++ 5_Trees_and_Graphs/RecoverBST.cpp
@@ -22,35 +22,30 @@ struct TreeNode {
  */
 class Solution {
 public:
-    TreeNode* first = nullptr;
-    TreeNode* second = nullptr;
-    // initialize prev element as INT_MIN val
-    TreeNode* prev = new TreeNode(INT_MIN);
+    TreeNode* first = nullptr;  // first uncool node
+    TreeNode* second = nullptr; // second uncool node
+    TreeNode* prev = new TreeNode(INT_MIN); // keep track of the previous node in inorder traversal
     
     void inorder(TreeNode* root) {
-        if(root == nullptr)
-            return;
+        if(root == nullptr) return;
         
         inorder(root->left);
         
-        // our business is to check out of order nodes
-        if(first == nullptr && prev->val > root->val)
+        if(first == nullptr && root->val < prev->val)
             first = prev;
         
-        if(first != nullptr && prev->val > root->val)
+        if(first != nullptr && root->val < prev->val)
             second = root;
         
         prev = root;
         
         inorder(root->right);
-
     }
+    
     void recoverTree(TreeNode* root) {
         inorder(root);
         
-        // swap the values of first and second
-        int temp = first->val;
-        first->val = second->val;
-        second->val = temp;
+        swap(first->val, second->val);
+        
     }
 };
