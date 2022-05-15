8ba1ca67bb05ce18456deff35a7493146e4a951c
akormous <iamakshatchauhan@gmail.com>
2022-05-15 23:24:31 +0530

    Add LC189

---
+++ 1_Arrays_and_Vectors/RotateArrayByK.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+BIG BRAIN SOLUTION
+*/
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k = k % nums.size();
+        
+        reverse(nums.begin(), nums.end());
+        
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
+    }
+};
