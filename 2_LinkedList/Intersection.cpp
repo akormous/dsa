636000bb8e87ecdd97e6cd751a71cbd37b741adc
akormous <iamakshatchauhan@gmail.com>
2022-06-06 09:35:54 +0530

    Add LC160

--- 2_LinkedList/Intersection.cpp
+++ 2_LinkedList/Intersection.cpp
@@ -8,6 +8,36 @@ Question: Find the intersecting point in two singly linked lists
 
 */
 
+/*
+EASY CLEAN
+*/
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* a = headA;
+        ListNode* b = headB;
+        
+        while(a != b) {
+            a = (a == nullptr ? headB : a->next);
+            b = (b == nullptr ? headA : b->next);
+        }
+        return a;
+    }
+};
+
 /* Link list Node */
 struct Node
 {
