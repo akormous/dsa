56693a476ebcf85c0052d71c78e131520d3efb30
akormous <iamakshatchauhan@gmail.com>
2022-09-13 13:31:37 +0530

    Add LC 123, 188

---
+++ 1_Arrays_and_Vectors/BuySellStock3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int k = 2;
+        int n = prices.size();
+        vector<vector<int>> dp(k+1, vector<int>(n, 0)); // dp array of size (k+1) x (n), initialized to 0
+        // dp[k][i] = maximum profits that can be achieved with k transactions and 0 .. i prices
+        // if we have 0 transactions to perform, then profits = 0
+        // if we are just at the 0th day, then profits = 0
+        
+        for(int t = 1; t <= k; t++) {   // for each number of transactions
+            int minPrice = prices[0];
+            
+            for(int i = 1; i < n; i++) {
+                dp[t][i] = max(dp[t][i-1], prices[i] - minPrice);
+                minPrice = min(minPrice, prices[i] - dp[t-1][i-1]);
+            }
+            
+        }
+        return dp[k][n-1];
+    }
+};
